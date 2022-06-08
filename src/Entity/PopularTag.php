7b5d43def4369427824fce81470d8683715b713e
mit2 <45004422+mit2pl@users.noreply.github.com>
2022-06-08 13:09:05 +0200

    try add Entity

---
+++ src/Entity/PopularTag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PrestaShop\Module\webo_taghint\Entity;
+
+use Doctrine\ORM\Mapping\Id;
+
+class PopularTag
+{
+    /**
+     * @var Id
+     * @ORM\Id
+     * @ORM\Column (name="id_popular_tag", type="integer")
+     * @ORM\GeneratedValue (strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     *
+     */
+
+}

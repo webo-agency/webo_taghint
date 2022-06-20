cee904295ac299e6ab6b79698c4bc17e8f5e374f
mit2 <45004422+mit2pl@users.noreply.github.com>
2022-06-20 17:01:55 +0200

    add table to admin panel

--- src/controllers/admin/AdminWeboTagHintController.php
+++ src/controllers/admin/AdminWeboTagHintController.php
@@ -12,12 +12,19 @@ class AdminWeboTagHintController extends ModuleAdminController
         $this->bootstrap = true;
         $this->id_lang = $this->context->language->id;
         $this->default_form_language = $this->context->language->id;
-        $this->addRowAction('edit');
-
+        $this->_defaultOrderBy = 'a.id';
+        $this->table = 'popular_tag';
+        $this->_select = 't.name as `name`';
+        $this->addRowAction('delete');
+        $this->_join = 'LEFT JOIN `'._DB_PREFIX_.'tag` t ON t.id_tag = a.id_tag';
+        $this->fields_list = [
+            'id' => ['title' => 'ID', 'class' => 'fixed-width-xs'],
+            'name' => ['title' => 'NAME']
+        ];
 //        Tools::redirectAdmin(Context::getContext()->link->getAdminLink('AdminModules').'&configure=webo_taghint');
     }
 
-    protected function getFromClause() {
-        return str_replace(_DB_PREFIX_, '', parent::getFromClause());
-    }
+//    protected function getFromClause() {
+//        return str_replace(_DB_PREFIX_, '', parent::getFromClause());
+//    }
 }
